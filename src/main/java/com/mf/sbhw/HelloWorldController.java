8035122ee552569e6cd3060eb1fea1e23ba94375
martin <martin@DESKTOP-T42JALR>
2023-10-30 16:17:23 +0100

    Migration to Spring.Boot 3 and JUnit 5.

--- src/main/java/com/mf/sbhw/HelloWorldController.java
+++ src/main/java/com/mf/sbhw/HelloWorldController.java
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+	/**
+	 * Usage: http://localhost:8080/hello?name=<<name>>
+	 * 
+	 * @param name
+	 * @return
+	 */
     @GetMapping("/hello")
     public String helloWorld(@RequestParam final String name) {
         return "Hello, " + name + ".\n";
--- src/test/java/com/mf/sbhw/ApplicationTest.java
+++ src/test/java/com/mf/sbhw/ApplicationTest.java
@@ -1,18 +1,17 @@
 package com.mf.sbhw;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 public class ApplicationTest {
 
     @Autowired
@@ -29,6 +28,6 @@ public class ApplicationTest {
                     get("/hello").param("name", "World"))
             .andExpect(status().isOk())
             .andExpect(
-                    content().string("Hello, World\n"));
+                    content().string("Hello, World.\n"));
     }
 }
